aa262b4bd2f1c495041771b5fad708f77f0f9158
dangoldmann <101563504+dangoldmann@users.noreply.github.com>
2022-06-03 12:28:31 -0300

    3/6

---
+++ Assets/Scripts/PlatformMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour
+{
+    bool ToRight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = 0.2f;
+        if (ToRight)
+        {
+            transform.position += new Vector3(speed, 0, 0);
+        }
+        else
+        {
+            transform.position -= new Vector3(speed, 0, 0);
+        }
+
+        if(transform.position.x >= 7)
+        {
+            ToRight = false;
+        }
+        else if (transform.position.x <= -7)
+        {
+            ToRight = true;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movementSpeed = 0.5f;
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-movementSpeed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(movementSpeed, 0, 0);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Platform")
+        {
+            //transform.position += new Vector3(0, 5, 0);
+        }
+        else if (col.gameObject.name == "FinalPlatform")
+        {
+            Debug.Log("Ganaste");
+        }
+    }
+}
